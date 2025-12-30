00fe3916c3f1f72248c05b715ed9e8cf6dd3b078
mjangoluan <mjangoluan@awesomelabph.com>
2025-12-30 17:26:27 +0800

    fix mongoose

--- server.js
+++ server.js
@@ -6,10 +6,7 @@ app.use(express.json());
 app.use(cors());
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
