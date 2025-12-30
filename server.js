bf38d36710c6ff5fb69404a5e822b4f4050f9ea5
mjangoluan <mjangoluan@awesomelabph.com>
2025-12-30 17:06:33 +0800

    update be

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(cors());
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI);
-
+console.log("Node version:", process.version);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
