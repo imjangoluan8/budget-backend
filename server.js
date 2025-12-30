279164eaf09c4621001f2d058e610446ed0e86f7
mjangoluan <mjangoluan@awesomelabph.com>
2025-12-30 17:21:29 +0800

    update mongoose

--- server.js
+++ server.js
@@ -5,8 +5,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI);
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
 console.log("Node version:", process.version);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
@@ -199,5 +205,3 @@ app.post("/transfer", async (req, res) => {
 
   res.json(created);
 });
-
-app.listen(3000, () => console.log("Server running on http://localhost:3000"));
